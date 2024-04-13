f087a6b10b4f0f42862bb08532608ca351173502
vamsisrinivasraavi <94818117+vamsisrinivasraavi@users.noreply.github.com>
2024-04-13 17:37:44 +0530

    added cloud services

---
+++ assets/js/navigation.js
@@ -0,0 +1,22 @@
+function navigateToCloudServices() {
+    window.location.href = 'cloud-services.html';
+}
+function navigateToCloudMigrationServices() {
+    window.location.href = "#CloudMigrationServices";
+}
+function navigateToCloudInfrastructureManagement() {
+    window.location.href = "#CloudInfrastructureManagement";
+}
+function navigateToCloudApplicationDevelopment() {
+    window.location.href = "#CloudApplicationDevelopment";
+}
+function navigateToCloudStorageandBackupSolutions() {
+    window.location.href = "#CloudStorageandBackupSolutions";
+}
+function navigateToCloudDeploymentandIntegrationServices() {
+    window.location.href = "#CloudDeploymentandIntegrationServices";
+}
+function navigateToCloudMonitoringandAnalytics() {
+    window.location.href = "#CloudMonitoringandAnalytics";
+}
+
